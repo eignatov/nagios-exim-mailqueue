cd8e7c2767faf08f27746e353483c5af5a7fce38
Andres Rosales <andres@infranetworking.com>
2013-11-21 09:38:40 -0200

    Changes

--- check_mailqueue.php
+++ check_mailqueue.php
@@ -1,33 +1,24 @@
 #!/usr/bin/php5
 <?php
 $number_mails=chop(shell_exec("/usr/bin/sudo /usr/sbin/exim -bpc"));
-
 $correosxdominios = chop(shell_exec('/usr/bin/sudo /usr/sbin/exim -bpr | grep "<" | awk {'."'print $4'".'} |cut -d "<" -f 2 | cut -d ">" -f 1 | sort -n | uniq -c| sort -n'));
 $drives = split("[\r|\n]", trim($correosxdominios));
-// Chuck away the unused first line
 array_shift($drives);
-
 $salida='';
 foreach ($drives as $key => $line){
-
         $line = trim(preg_replace('/\s+/',' ', $line));
         $linearray = explode(' ',$line);
-        //print_r($linearray);
         foreach($linearray as $keyl => $linea){
-                //echo $keyl." ".$linea."\n";
                 if($keyl == 0){
                         $salida.="($linea)";
                 }
-
                 if ($keyl ==1){
                         $salida.=$linea." ";
                 }
         }
-
 }
 
 $correosxdominios=$salida;
-
 switch ($number_mails) {
         case $number_mails < 200:
         echo "OK - $number_mails correos en la cola de salida.";
